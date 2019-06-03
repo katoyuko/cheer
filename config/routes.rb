ffb5b633f9f48abdbae2622b5a2f494e2a91783b
Yuko <naka.u926@gmail.com>
2019-06-03 07:04:13 +0000

    [Modify]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
 
   root "posts/top"
 
-  devise_for :users, controllers: {
-    sessions: "users/sessions",
-    passwords: "users/passwords",
-    registrations: "users/registrations"
-  }
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
