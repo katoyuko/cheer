c5285f1b79c4c9c6219a0fe80e7403988c4cb067
Yuko <naka.u926@gmail.com>
2019-06-06 07:14:21 +0000

    [Add]rails g rspec:install

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
+
+  # PostsControllerをテスト対象とする
 RSpec.describe PostsController, type: :controller do
+
+  describe "新規投稿ページ" do
+    context "新規投稿ページが正しく表示される"
+      beforedo
+        get :new
+      end
+      it "returns http success 200OKとなること" do
+        expect(response.status).to eq200
+    end
+  end
 
   describe "GET #index" do
     it "returns http success" do
@@ -23,12 +35,6 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
 
   describe "GET #create" do
     it "returns http success" do
