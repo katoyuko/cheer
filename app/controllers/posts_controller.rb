3fd886d58c45cd7c6a9d7bc28be83460b64f1cec
Yuko <naka.u926@gmail.com>
2019-06-07 13:04:27 +0000

    [Update]posts index&show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,10 +25,11 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse_order
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
