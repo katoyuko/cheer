2edc7c557009fd9cb3ac3e81e8a187ec85b3c70e
Yuko <naka.u926@gmail.com>
2019-06-08 14:45:16 +0000

    [Add]post_comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 
-
-  get 'post_comments/create'
   root "posts#top"
   get "/about" => "posts#about"
 
-  get 'post_categories/show'
-
   devise_for :users
   resources :users, only: [:show, :edit, :update, :destroy]
-  resources :posts
   resources :post_categories, only: [:create, :show]
+  resources :posts do
+    resource :post_comments, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
