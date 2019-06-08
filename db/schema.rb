f36004c051bb33f0923d024f70f94927dbbc9fc2
Yuko <naka.u926@gmail.com>
2019-06-08 14:35:27 +0000

    [Add]post_comment association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_074959) do
+ActiveRecord::Schema.define(version: 2019_06_08_142519) do
 
   create_table "post_categories", force: :cascade do |t|
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
