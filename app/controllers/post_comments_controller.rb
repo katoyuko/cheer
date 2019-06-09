927969710800f3028adfa5c7c44d66dafa7ac670
Yuko <naka.u926@gmail.com>
2019-06-09 00:38:16 +0000

    [Add]controller,view for post_comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,18 @@
 class PostCommentsController < ApplicationController
   def create
+    post = Post.find(params[:post_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post)
   end
 
   def destroy
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment, :user_id, :post_id)
   end
 end
