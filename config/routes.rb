bfbd5d4dd6528d15568fd5abe8cb089653400574
Yuko <naka.u926@gmail.com>
2019-06-09 12:18:17 +0000

    [Add]cheer for routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :post_categories, only: [:create, :show]
   resources :posts do
     resource :post_comments, only: [:create, :destroy]
+    resource :cheers, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
