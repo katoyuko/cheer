736167f9c0b14661c064aada80f1157b9de96a4c
Yuko <naka.u926@gmail.com>
2019-06-10 00:50:05 +0000

    [Add]favorite_category routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:show, :edit, :update, :destroy]
-  resources :post_categories, only: [:create, :show]
+
+  resources :post_categories, only: [:create, :show] do
+    resource :favorite_categories, only: [:create, :destroy]
+  end
+
   resources :posts do
     resource :post_comments, only: [:create, :destroy]
     resource :post_cheers, only: [:create, :destroy]
