460107df7306281fa2635b34fe322658f8c0ef09
Yuko <naka.u926@gmail.com>
2019-06-10 07:28:28 +0000

    [Add]routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
   root "posts#top"
   get "/about" => "posts#about"
 
-  devise_for :admins
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    registrations: "users/registrations"
+  }
+
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    registrations: "admins/registrations"
+  }
+
   resources :users, only: [:show, :edit, :update, :destroy]
 
   resources :post_categories, only: [:create, :show]
@@ -13,6 +23,12 @@ Rails.application.routes.draw do
   resources :posts do
     resource :post_comments, only: [:create, :destroy]
     resource :post_cheers, only: [:create, :destroy]
+  end
+
+  namespace :admins do
+    resources :posts, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :destroy]
+    resources :post_categories, only: [:index, :show, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
