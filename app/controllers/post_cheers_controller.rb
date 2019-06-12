088abdd770735358c293401b066a5454e0172c97
Yuko <naka.u926@gmail.com>
2019-06-12 03:01:02 +0000

    [Update]post_cheer_controller

--- app/controllers/post_cheers_controller.rb
+++ app/controllers/post_cheers_controller.rb
@@ -3,17 +3,18 @@ class PostCheersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    post_cheer = current_user.post_cheers.new(post_id: post.id)
-    post_cheer.save
-    redirect_to post_path(post)
+    @post_cheer = PostCheer.create(user_id: current_user.id, post_id: params[:post_id])
+    # cheerの合計数を表示
+    @post_cheers = PostCheer.where(post_id: params[:post_id])
+    # 投稿一覧ページを表示
+    @posts = Post.all
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    post_cheer = current_user.post_cheers.find_by(post_id: post.id)
+    post_cheer = PostCheer.find_by(user_id: current_user.id, post_id: params[:post_id])
     post_cheer.destroy
-    redirect_to post_path(post)
+    @post_cheers = PostCheer.where(post_id: params[:post_id])
+    @posts = Post.all
   end
 
 end
