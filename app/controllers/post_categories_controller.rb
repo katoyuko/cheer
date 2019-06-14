814913042f52402daa6a8b04ea41a79bf0773276
Yuko <naka.u926@gmail.com>
2019-06-14 04:07:53 +0000

    [Update]pagenation on category show page

--- app/controllers/post_categories_controller.rb
+++ app/controllers/post_categories_controller.rb
@@ -2,7 +2,7 @@ class PostCategoriesController < ApplicationController
 
   def show
     @post_category = PostCategory.find(params[:id])
-    @posts = @post_category.posts.page(params[:page]).reverse_order.per(12)
+    @posts = @post_category.posts.page(params[:page]).reverse_order.per(20)
   end
 
 private
