980fbe1cf0372ac6cab989317d8e1545f510f9e6
Yuko <naka.u926@gmail.com>
2019-06-19 03:10:11 +0000

    [Modify]user/index, show for admin

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -13,16 +13,31 @@ class Admins::UsersController < ApplicationController
   end
 
   def show
+    @post = Post.new
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).reverse_order.per(8)
-    # お気に入りカテゴリ一覧
-    @favorite_categories = @user.favorite_categories.page(params[:page]).reverse_order.per(4)
+    @posts = @user.posts.page(params[:page]).reverse_order.per(12)
+
+    # お気に入りカテゴリ
+    @favorite_category = FavoriteCategory.new
+    @favorite_categories = @user.favorite_categories.page(params[:page]).reverse_order
+
+    # チャート
+    @chart_data = {}
+    posts = @user.posts.all
+    posts.each do |post|
+      date = post.created_at.strftime('%Y/%m/%d')
+      if @chart_data.has_key?(date)
+        @chart_data[date] += 1
+      else
+        @chart_data[date] = 1
+      end
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admins_users_path
+    redirect_to admins_users_path, notice: "削除しました"
   end
 
 end
