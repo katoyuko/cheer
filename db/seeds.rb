e6a0a39d3b0f57d72d0cfee71311f5ec54b652ce
Yuko <naka.u926@gmail.com>
2019-06-21 16:27:19 +0000

    [Update]about page (add hou to use)

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,48 @@
 
 # 投稿カテゴリ
 
+require "csv"
+
+CSV.foreach('db/category.csv', headers: true) do |row|
+  PostCategory.create(category: row["category"])
+end
 
 
-100.times do
+50.times do
   Post.create(
-    post_category_id: Faker::Number.within(1..13),
+    post_category_id: Faker::Number.within(1..12),
     post_content: '頑張ります',
-    user_id: Faker::Number.within(4..5),
+    user_id: Faker::Number.within(1..50),
     created_at: Faker::Date.between(1.year.ago, Date.today),
     cheer_count: Faker::Number.within(0..50)
       )
 end
 
+
+50.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password
+               )
+end
+
+
+50.times do
+  PostComment.create(
+    comment: '応援してます',
+    user_id: Faker::Number.within(1..50),
+    post_id: Faker::Number.within(1..100)
+      )
+end
+
+
+4.times do
+  FavoriteCategory.create(
+    post_category_id: Faker::Number.within(1..12),
+    user_id: Faker::Number.within(1..50)
+      )
+end
