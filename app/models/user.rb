e0d089320b701bc19688ffadf901ae7f4df7fd47
Yuko <naka.u926@gmail.com>
2019-06-26 00:51:48 +0000

    [Add]Regular Expression for mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
 
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :post_cheers, dependent: :destroy
-
   has_many :favorite_categories, dependent: :destroy
 
   attachment :user_image
